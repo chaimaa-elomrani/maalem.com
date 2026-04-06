5d2b816c9f1a3d5fc7a887fc04816d40f7c38f40
sheima <chaimaelomrani6@gmail.com>
2026-04-06 21:45:52 +0100

    added the functions needed in the posts controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::with('user')->latest()->paginate(12);
+        return view('feed', compact('posts'));
+    }
+
+    public function create(){
+        
+        return view('posts.create'); 
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title'=> 'required|string|max:255',
+            'description'=> 'required|string',
+            'images'=> 'required|array',
+            'images.*'=> 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            'category'=> 'required|string',
+            'tags'=> 'required|array',
+            'tags.*'=> 'string',
+        ]);
+        $post = Post::create([
+            'title'=> $request->title,
+            'description'=> $request->description,
+            'images'=> $request->images,
+            'category'=> $request->category,
+            'tags'=> $request->tags,
+        ]);
+
+        return redirect()->route('feed')->with('success','');
+    }
 }
