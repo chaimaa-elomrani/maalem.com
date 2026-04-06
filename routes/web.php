9f89a04e92a893988a7030d5164aba786a103cf8
sheima <chaimaelomrani6@gmail.com>
2026-04-06 21:47:25 +0100

    added the routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('posts/create', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
 });
+
+Route::get('/feed', [PostController::class, 'index'])->name('feed');
 
 require __DIR__.'/auth.php';
